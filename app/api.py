085a6d806a804ef5169425e760753be38077494f
Carlos <carlos@riv.as>
2017-08-12 14:17:01 -0700

    "Flaskifying" the index.html template and adding preliminary config.py skeleton

--- app/api.py
+++ app/api.py
@@ -1,9 +1,16 @@
+import os
 import time
+
 from flask import Flask, jsonify
 from flask import render_template
 from werkzeug.contrib.cache import SimpleCache
 
-app = application = Flask("VueFlask")
+APP_ROOT_FOLDER = os.path.abspath(os.path.dirname(__file__))
+TEMPLATE_FOLDER = os.path.join(APP_ROOT_FOLDER, 'templates')
+STATIC_FOLDER = os.path.join(APP_ROOT_FOLDER, 'static')
+
+app = Flask("VueFlask", template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLDER)
+
 cache = SimpleCache()
 
 
---
+++ app/config.py
@@ -0,0 +1,10 @@
+class Base(object):
+    pass
+
+
+class Testing(Base):
+    pass
+
+
+class Production(Base):
+    pass
